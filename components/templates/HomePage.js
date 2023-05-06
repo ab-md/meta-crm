82d017cef661cefc5ed12ca248df31bb4b2bdb82
Abolfazl Madadi <metafite800@gmail.com>
2023-05-06 16:59:43 +0430

    Add some features

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import Card from '../modules/Card';
 
@@ -6,9 +7,14 @@ const HomePage = ({ data }) => {
     return (
         <div className="min-h-screen">
             {
-                data?.map(customer => (
+                data.length ? data.map(customer => (
                     <Card key={customer._id} data={customer} />
-                ))
+                )) : (
+                    <div className="font-bold text-xl h-64 flex flex-col justify-center items-center">
+                        <p>No Customers Yet?</p>
+                        <Link href="/add-customer" className="text-blue-400 cursor-pointer">Add Some</Link>
+                    </div>
+                )
             }
         </div>
     );
